9001552cf12bbe62da9501bfe917fd6dd1f0807b
maro114510 <atumaro114510@icloud.com>
2024-01-01 23:56:39 +0900

    change from pre-commit to merge

--- discord/main.py
+++ discord/main.py
@@ -1,62 +1,72 @@
 
+import requests
 import os
 from io import StringIO
 from datetime import datetime, timedelta
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
 TELLER_TOKEN = os.getenv("TELLER_TOKEN")
+HORI_WEBHOOK= os.getenv("HORI_WEBHOOK")
+TEST_WEBHOOK_URL = os.getenv("TEST_WEBHOOK")
 
 intents = discord.Intents.default()
-intents.members = True 
-intents.message_content = True 
+intents.members = True
+intents.message_content = True
 
-
-# Botをインスタンス化
+# Instantiate bot
 bot = commands.Bot(
-    command_prefix="$", 
-    case_insensitive=True, 
-    intents=intents 
+	command_prefix="$",
+	case_insensitive=True,
+	intents=intents
 )
 
 @bot.event
 async def on_ready():
-    print("Bot is ready!")
-
-
-@bot.command()
-async def hello(ctx: commands.Context) -> None:
-    """helloと返すコマンド"""
-    await ctx.send(f"Hello {ctx.author.name}")
-
-@bot.command()
-async def add(ctx: commands.Context, a: int, b: int) -> None:
-    """足し算をするコマンド"""
-    await ctx.send(a+b)
-
+	print("Bot is ready!")
 
 @bot.command(
-    name="message",
-    aliases=["msg", "m"],
+	name="message",
+	aliases=["msg", "m"],
+	description="Get messages from a channel and send them to a webhook."
 )
 async def get_message(ctx: commands.Context, channel: discord.TextChannel) -> None:
-    """チャンネルのメッセージを取得し、テキストファイルに保存するコマンド"""
+	"""Get messages from a channel and send them to a webhook."""
 
-    stream = StringIO()
+	stream = StringIO()
+	messages = []
 
-    async for message in channel.history(
-        after=datetime.utcnow() -timedelta(hours=1),
-        oldest_first=True, 
-    ):
-        jst = message.created_at + timedelta(hours=9)
-        msg = f"{message.author.name}: {jst.strftime('%Y/%m/%d %H:%M:%S')}\n{message.content}"
-        stream.write(msg)
-        stream.write("\n\n")
+	async for message in channel.history(
+		after=datetime.utcnow() - timedelta(hours=24),
+		oldest_first=True,
+	):
+		jst = message.created_at + timedelta(hours=9)
+		org_channel = channel.name
+		msg = f"""{message.author.name}: {jst.strftime('%Y/%m/%d %H:%M:%S')}\n{message.content}\norigin: {org_channel}"""
+		stream.write(msg)
+		stream.write("\n\n")
+		messages.append(msg)
+	#--- end of loop ---
 
-    stream.seek(0)
-    await ctx.send(file=discord.File(stream, filename="messages.txt"))
-    stream.close()
+	stream.seek(0)
+	await ctx.send(file=discord.File(stream, filename="messages.txt"))
+	stream.close()
+
+	for msg in messages:
+		res = requests.post(
+			TEST_WEBHOOK_URL,
+			json={
+				"content": msg,
+			}
+		)
+		if res.status_code != 204:
+			print("Error: ", res.status_code)
+		elif res.status_code == 204:
+			print("Success")
+		#--- end of if ---
+	#--- end of loop ---
+#--- end of def ---
 
 bot.run(TELLER_TOKEN)
